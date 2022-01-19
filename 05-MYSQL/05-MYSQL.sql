c8fadd6331e03ec783b4e42c53091791b02b7cae
NahuelTolosa <josetolosa98@gmail.com>
2022-01-19 10:18:25 -0300

    Mysql

---
+++ 05-MYSQL/05-MYSQL.sql
@@ -0,0 +1,30 @@
+create database if not exists prueba;
+
+use prueba;
+
+ 
+create table categories (
+	id_category int auto_increment not null,
+    category varchar(40) not null,
+    constraint pk_id_category primary key (id_category)
+);
+
+insert into categories ( category ) values
+( "Harina" ),
+( " Lácteos" );
+
+
+
+create table items (
+	id_item int auto_increment not null,
+    item_name varchar(40) not null,
+    category int not null,
+    stock int unsigned not null,
+    constraint pk_id_item primary key (id_item),
+    constraint fk_category foreign key (category) references categories(id_category)
+);
+
+insert into items ( item_name, category, stock ) values
+( "Fideos", 1, 20 ),
+( "Leche", 2, 30 ),
+( "Crema", 2, 30 );
